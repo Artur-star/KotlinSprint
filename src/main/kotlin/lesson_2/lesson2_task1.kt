49a2258121d6c667b64445dcf6000b2d8e2e24af
Artur <ArtSoull@yandex.ru>
2024-04-23 23:13:42 +0500

    KS-2-1 removed the magic numbers and added toFloat

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,6 +1,13 @@
 package lesson_2
 
 fun main() {
-    val averageScore:Double = (3 + 4 + 3 + 5)/4.0
+    val student1 = 3
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+
+    val averageScore: Float = ((student1 + student2 + student3 + student4) / 4)
+        .toFloat()
+
     println(averageScore)
 }
