4ddec63695b23b0eed68ee0688935bd9741b9b21
Artut <ArtSoull@yandex.ru>
2024-04-23 19:08:54 +0500

    KS-1-3 edit 1, changed the number to a string and deleted the extra change

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,11 +1,14 @@
 package lesson_1
 
 fun main() {
-    val yearFlightGagarin = 1961
-    var hourFlightGagarin = 9
-    var minuteFlightGagarin = 7
-    println("$yearFlightGagarin\n0$hourFlightGagarin\n0$minuteFlightGagarin")
-    hourFlightGagarin = 10
-    minuteFlightGagarin = 55
+    val yearFlightGagarin = "1961"
+    var hourFlightGagarin = "09"
+    var minuteFlightGagarin = "07"
+
+    println("$yearFlightGagarin\n$hourFlightGagarin\n$minuteFlightGagarin")
+
+    hourFlightGagarin = "10"
+    minuteFlightGagarin = "55"
+
     println("$hourFlightGagarin:$minuteFlightGagarin")
 }
