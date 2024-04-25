85e29192f40d88d35bfdb4cfe4f36e256d6faeef
Artur <ArtSoull@yandex.ru>
2024-04-25 22:42:56 +0500

    KS-3-3 calculator

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,19 @@
+package lesson_3
+
+fun main() {
+    val figure = 6
+
+    val printMultiplication = """
+        |$figure * 1 = ${figure * 1}
+        |$figure * 2 = ${figure * 2}
+        |$figure * 3 = ${figure * 3}
+        |$figure * 4 = ${figure * 4}
+        |$figure * 5 = ${figure * 5}
+        |$figure * 6 = ${figure * 6}
+        |$figure * 7 = ${figure * 7}
+        |$figure * 8 = ${figure * 8}
+        |$figure * 9 = ${figure * 9}
+    """.trimMargin()
+
+    println(printMultiplication)
+}
