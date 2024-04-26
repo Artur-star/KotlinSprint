ef9a13cb660ae66535f6c11e60de55c8dc55c1fb
Artut <ArtSoull@yandex.ru>
2024-04-26 14:25:54 +0500

    KS-3-4 Task chess

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -1,19 +1,16 @@
 package lesson_3
 
 fun main() {
-    val figure = 9
 
-    println(
-        """
-        |$figure * 1 = ${figure * 1}
-        |$figure * 2 = ${figure * 2}
-        |$figure * 3 = ${figure * 3}
-        |$figure * 4 = ${figure * 4}
-        |$figure * 5 = ${figure * 5}
-        |$figure * 6 = ${figure * 6}
-        |$figure * 7 = ${figure * 7}
-        |$figure * 8 = ${figure * 8}
-        |$figure * 9 = ${figure * 9}
-    """.trimMargin()
-    )
+    var from = "E2"
+    var to = "E4"
+    var numberMove = 1
+
+    println("$from:$to, $numberMove")
+
+    from = "D2"
+    to = "D3"
+    numberMove = 2
+
+    println("$from:$to, $numberMove")
 }
