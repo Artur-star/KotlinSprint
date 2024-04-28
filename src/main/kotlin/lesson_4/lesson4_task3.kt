a163723424ca6bf5b6356d04422ea892eff0c55f
Artut <ArtSoull@yandex.ru>
2024-04-28 16:13:23 +0500

    KS-4-3 Refactoring

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,13 +1,21 @@
 package lesson_4
 
+const val SUNNY_WEATHER = true
+const val ROOF_OPEN = true
+const val AIR_HUMIDITY_REQUIRED = 20
+const val TIME_YEAR = "notWinter"
+
 fun main() {
     val weatherToday = true
-    val coveringMaterialAtMoment = true
-    val airHumidity = 20
-    val currentTimeOfYear = "winter"
+    val roofOpenNow = true
+    val airHumidityNow = 20
+    val currentTimeOfYear = "notWinter"
 
     println(
         "Are the conditions favorable for the growth of legumes now? " +
-                "${weatherToday && coveringMaterialAtMoment && airHumidity == 20 && currentTimeOfYear == "notWinter"}"
+                "${
+                    SUNNY_WEATHER == weatherToday && ROOF_OPEN == roofOpenNow &&
+                            AIR_HUMIDITY_REQUIRED == airHumidityNow && TIME_YEAR == currentTimeOfYear
+                }"
     )
 }
