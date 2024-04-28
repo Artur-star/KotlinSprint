c9dfce5b39eaf319afd03580aab1fc2f1f01a439
Artut <ArtSoull@yandex.ru>
2024-04-28 17:29:17 +0500

    KS-4-5 Checking for null, set via readln()

--- src/main/kotlin/lesson_4/lesson4_tast5.kt
+++ src/main/kotlin/lesson_4/lesson4_tast5.kt
@@ -1,20 +1,21 @@
 package lesson_4
 
 fun main() {
-    print("The ship has no damage: ")
-    val noDamage = readLine().toBoolean()
+    println("The ship has no damage: ")
+    val noDamage = readln().toBoolean()
 
-    print("Number of crew: ")
-    val numberCrew = readLine()?.toInt()
+    println("Number of crew: ")
+    val numberCrew = readln().toInt()
 
-    print("Number of boxes: ")
-    val numberBoxes = readLine()?.toInt()
+    println("Number of boxes: ")
+    val numberBoxes = readln().toInt()
 
-    print("weather conditions: ")
-    val weatherConditions = readLine().toBoolean()
+    println("weather conditions: ")
+    val weatherConditions = readln().toBoolean()
 
     if (((noDamage && numberCrew in 55..70 && weatherConditions)
-                || (!noDamage && numberCrew == 70)) && numberBoxes!! >= 50) {
+                || (!noDamage && numberCrew == 70)) && numberBoxes >= 50
+    ) {
         println("Ship can sail")
     } else println("Ship can not sail")
 }
