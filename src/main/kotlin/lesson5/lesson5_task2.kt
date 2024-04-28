9e3a5a896c56c4631575777bace09ef2ead0c37b
Artut <ArtSoull@yandex.ru>
2024-04-28 18:12:51 +0500

    KS-5-2 The task of coming of age, added a null check via readln()

--- src/main/kotlin/lesson5/lesson5_task2.kt
+++ src/main/kotlin/lesson5/lesson5_task2.kt
@@ -7,9 +7,8 @@ const val COMING_AGE = 18
 fun main() {
     print("Введите ваш год рождения: ")
 
-    val yearBirth = readLine()?.toInt()
+    val yearBirth = readln().toInt()
     val currentYear = LocalDate.now().year
 
-    if (currentYear - yearBirth!! >= COMING_AGE) println("Показать экран со скрытым контентом")
-
+    if (currentYear - yearBirth >= COMING_AGE) println("Показать экран со скрытым контентом")
 }
