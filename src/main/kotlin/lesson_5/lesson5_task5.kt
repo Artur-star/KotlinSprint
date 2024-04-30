7d3802784c9f7962fe2571b7f7b042f7bd26395a
Artut <ArtSoull@yandex.ru>
2024-04-30 13:57:20 +0500

    KS-5-5 Fixed bugs: removed destructuring, added hints to the user

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -3,20 +3,23 @@ package lesson_5
 fun main() {
     val minNumber = 0
     val maxNumber = 42
-    val firstRandomNumber: Int = (minNumber..maxNumber).random()
-    val secondRandomNumber: Int = (minNumber..maxNumber).random()
-    val threeRandomNumber: Int = (minNumber..maxNumber).random()
 
-    val listRandomNumbers = listOf(firstRandomNumber, secondRandomNumber, threeRandomNumber)
+    val listRandomNumbers = listOf(
+        (minNumber..maxNumber).random(),
+        (minNumber..maxNumber).random(),
+        (minNumber..maxNumber).random()
+    )
+    println("Введите три числа: ")
     val listUserNumbers = listOf(readln().toInt(), readln().toInt(), readln().toInt())
     val intersect = listRandomNumbers.intersect(listUserNumbers)
+    val sizeCollection = intersect.size
 
-    when (intersect.size) {
+    when (sizeCollection) {
         3 -> println("Ты угадал все числа и выиграл джекпот!!!")
         2 -> println("Ты угадал два числа и получает крупный приз!")
         1 -> println("Ты угадал одно число и получаешь утешительный приз")
         else -> println("К сожалению угаданных чисел нет")
     }
 
-    println("Выигрышные чмсла были $listRandomNumbers")
+    println("Выигрышными числа были: $listRandomNumbers")
 }
