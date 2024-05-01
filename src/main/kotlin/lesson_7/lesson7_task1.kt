2e7ec597af1e7dab930d1ba944bc63012639c614
Artut <ArtSoull@yandex.ru>
2024-05-01 19:18:22 +0500

    KS-7-4 Change 1

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -2,8 +2,12 @@ package lesson_7
 
 fun main() {
     var password = ""
+    var randomChar: Char
+    var randomInt: Int
     for (i in 1..3) {
-        password += "${('a'..'z').random()}${(1..9).random()}"
+        randomChar = ('a'..'z').random()
+        randomInt = (1..9).random()
+        password += "$randomChar$randomInt"
     }
     println(password)
 }
