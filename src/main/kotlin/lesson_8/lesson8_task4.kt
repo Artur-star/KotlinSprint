50db1c99eef5393375e1cca62d2043254793f333
Artut <ArtSoull@yandex.ru>
2024-05-02 19:36:42 +0500

    KS-8-4 Replacing an ingredient in an array

---
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -0,0 +1,22 @@
+package lesson_8
+
+fun main() {
+    val listIngredients = arrayOf("соль", "сахар", "молоко", "мука", "яйцо куриное", "растительное масло")
+
+    println("Начальный список ингридиетов: ")
+    for (i in listIngredients) {
+        print("$i ")
+    }
+
+    print("\nКакой ингридиент ты хочешь найти: ")
+    val userRequest = readln()
+
+    if (listIngredients.contains(userRequest)) {
+        println("Каким ингридиентом ты его хочешь заменить: ")
+        listIngredients[listIngredients.indexOf(userRequest)] = readln()
+        println("Готово! Вы сохранили следующий список: ")
+        for (i in listIngredients) {
+            print("$i ")
+        }
+    } else println("Такого ингредиента в рецепте нет")
+}
