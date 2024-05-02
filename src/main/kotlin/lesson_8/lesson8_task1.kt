fe699280630dea867ff8764778b49ac7f7dead22
Artut <ArtSoull@yandex.ru>
2024-05-02 15:17:48 +0500

    KS-8-1 Applied the function 'sum'

--- src/main/kotlin/lesson_8/lesson8_task1.kt
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -1,10 +1,7 @@
 package lesson_8
 
 fun main() {
-    val arrayNumberAds = arrayOf(10, 14, 5, 34, 65, 34, 65)
-    var weeklyViews: Int = 0
-    for (i in arrayNumberAds) {
-        weeklyViews += i
-    }
-    println(weeklyViews)
+    val arrayNumberAds = arrayOf(10, 10, 10, 10, 10, 10, 10)
+    val sum = arrayNumberAds.sum()
+    println(sum)
 }
