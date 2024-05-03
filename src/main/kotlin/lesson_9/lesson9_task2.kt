8f827b5a18a4706ee335ffe976b9f0794af791ae
Artut <ArtSoull@yandex.ru>
2024-05-03 11:04:09 +0500

    KS-9-2 Create a list of ingredients

---
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -0,0 +1,14 @@
+package lesson_9
+
+fun main() {
+    val listOfIngredients = mutableListOf("соль", "сахар", "мука")
+
+    println("В рецепте есть базовые ингредиенты: ${listOfIngredients.joinToString()}")
+    println("Желаете добавить еще?")
+
+    if(readln() == "да") {
+        println("Какой ингредиент вы хотите добавить?")
+        listOfIngredients.add(readln())
+        println("Теперь в рецепте есть следующие ингредиенты: ${listOfIngredients.joinToString()}")
+    }
+}
