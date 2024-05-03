b581fcbe91ad0dcbed82781b5eb88e3085fbc017
Artut <ArtSoull@yandex.ru>
2024-05-03 09:24:20 +0500

    KS-7-5 Changed the array to a list, added a method shuffled()

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -1,20 +1,28 @@
 package lesson_7
 
 fun main() {
-
     val numberRange = 0..9
     val lowercaseCharRange = 'a'..'z'
     val capitalCharRange = 'A'..'Z'
     val minSizePassword = 6
-    var password = ""
+    val password = mutableListOf<String>()
     var enterUser: Int
+
     do {
         println("Введи длину пароля, пароль должен быть не менее 6 знаков: ")
         enterUser = readln().toInt()
     } while (enterUser < minSizePassword)
 
-    for (i in 1..enterUser) {
-        password += "${numberRange.random()}${lowercaseCharRange.random()}${capitalCharRange.random()}".random()
+    password.add("${numberRange.random()}")
+    password.add("${lowercaseCharRange.random()}")
+    password.add("${capitalCharRange.random()}")
+    for (i in password.size..enterUser) {
+        password.add(
+            "${numberRange.random()}${lowercaseCharRange.random()}${capitalCharRange.random()}".random().toString()
+        )
     }
-    println(password)
+    val shuffledPassword = password.shuffled()
+    println(shuffledPassword)
+
+
 }
