e24c8a7e79775f49f2482d74cad053f1d7798dc0
Artut <ArtSoull@yandex.ru>
2024-05-04 16:09:32 +0500

    KS-8-4 Add fun indexOf()

--- src/main/kotlin/lesson_8/lesson8_task4.kt
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -15,8 +15,8 @@ fun main() {
         println("Каким ингридиентом ты его хочешь заменить: ")
         listIngredients[listIngredients.indexOf(userRequest)] = readln()
         println("Готово! Вы сохранили следующий список: ")
-        for (i in listIngredients) {
-            print("$i ")
+        for (i in 0..listIngredients.indexOf(listIngredients.max())) {
+            println(listIngredients[i])
         }
     } else println("Такого ингредиента в рецепте нет")
 }
