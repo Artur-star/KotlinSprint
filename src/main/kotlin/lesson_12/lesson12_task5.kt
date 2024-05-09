ca64e8936fb45352c0b9601065bf2b67112b6cfd
Artut <ArtSoull@yandex.ru>
2024-05-09 17:46:10 +0500

    KS-12-5 Correct1

--- src/main/kotlin/lesson_12/lesson12_task5.kt
+++ src/main/kotlin/lesson_12/lesson12_task5.kt
@@ -1,46 +1,33 @@
 package lesson_12
 
-import java.util.Random
-
-val LIST_DAY_TIME_TEMP = mutableListOf<Int>()
-val LIST_NIGHT_TEMP = mutableListOf<Int>()
-val LIST_IS_RAINFALL = mutableListOf<Boolean>()
+import kotlin.random.Random
 
 class Weather5(
     val daytimeTemperature: Int,
     val nightTemperature: Int,
     val isRainfall: Boolean,
-) {
-
-    init {
-        createListForEachField()
-    }
-
-    private fun createListForEachField() {
-        LIST_IS_RAINFALL.add(isRainfall)
-        LIST_DAY_TIME_TEMP.add(daytimeTemperature)
-        LIST_NIGHT_TEMP.add(nightTemperature)
-    }
-
-    companion object {
-        fun calculateAverageTemperatures() {
-            System.out.printf("Средняя температура ночью за 30 дней: %.1f\n", LIST_NIGHT_TEMP.average())
-            System.out.printf("Средняя температура днем за 30 дней: %.1f\n", LIST_DAY_TIME_TEMP.average())
-            println("Количество дней с осадками: ${LIST_IS_RAINFALL.groupingBy { it }.eachCount()[true]}")
-        }
-    }
-}
+)
 
 fun main() {
-    val mutableListOf = mutableListOf<Weather5>()
+    val listDayTemp = mutableListOf<Int>()
+    val listNightTemp = mutableListOf<Int>()
+    val listIsRainfall = mutableListOf<Boolean>()
+
+    val weatherListForThirtyDays = mutableListOf<Weather5>()
     for (i in 0..<30) {
-        mutableListOf.add(
+        weatherListForThirtyDays.add(
             Weather5(
                 daytimeTemperature = (20..35).random(),
                 nightTemperature = (17..25).random(),
-                isRainfall = Random().nextBoolean()
+                isRainfall = Random.nextBoolean()
             )
         )
+        listDayTemp.add(weatherListForThirtyDays[i].daytimeTemperature)
+        listNightTemp.add(weatherListForThirtyDays[i].nightTemperature)
+        listIsRainfall.add(weatherListForThirtyDays[i].isRainfall)
     }
-    Weather5.calculateAverageTemperatures()
+
+    System.out.printf("Средняя температура ночью за 30 дней: %.1f\n", listNightTemp.average())
+    System.out.printf("Средняя температура днем за 30 дней: %.1f\n", listDayTemp.average())
+    println("Количество дней с осадками: ${listIsRainfall.groupingBy { it }.eachCount()[true]}")
 }
