3f4f53495fb08b68a217c902fca3766f6ea5ca5a
Artut <ArtSoull@yandex.ru>
2024-05-10 16:22:31 +0500

    KS-10-4 Fixed1

--- src/main/kotlin/lesson_10/lesson10_task4.kt
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -1,26 +1,24 @@
 package lesson_10
 
 fun main() {
-    println("Человек выиграл ${round()} ")
+    var count = 0
+    do {
+        if (conductRound()) {
+            println("Humanity has won")
+            count++
+        }
+        println("Хотите бросить кости еще раз? Введите Да или Нет")
+    } while (readln().equals("да", ignoreCase = true))
+
+    println("Человек выиграл $count")
 }
 
 fun rollingDice(): Int = (1..6).random()
 
-fun round(): Int {
-    var count = 0
-    do {
-        val playerThrow = rollingDice()
-        println("the player's move: $playerThrow")
-        val computerThrow = rollingDice()
-        println("the computer's move: $computerThrow")
-
-        if (playerThrow > computerThrow) {
-            println("Humanity has won")
-            count++
-        } else if (playerThrow < computerThrow) println("The robots have won")
-        else println("The rivalry continues")
-
-        println("Хотите бросить кости еще раз? Введите Да или Нет")
-    } while (readln().lowercase() == "да")
-    return count
+fun conductRound(): Boolean {
+    val playerThrow = rollingDice()
+    println("the player's move: $playerThrow")
+    val computerThrow = rollingDice()
+    println("the computer's move: $computerThrow")
+    return playerThrow > computerThrow
 }
