2d7d7467e1daa6a2349271ca57c91d5b218efafe
Artut <ArtSoull@yandex.ru>
2024-05-10 09:30:46 +0500

    KS-13-4 Phone directory4

---
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -0,0 +1,33 @@
+package lesson_13
+
+class PhoneDirectory4(
+    var name: String,
+    var phoneNumber: Long?,
+    var company: Company?,
+) {
+    override fun toString(): String {
+        return "\n- name: $name\n- phoneNumber: ${phoneNumber ?: "Введите номер телефона, контакт не попадет в телефонную книгу"}\n- company: ${company?.name}"
+    }
+}
+
+class Company(
+    var name: String
+)
+
+fun main() {
+    val listBeforeFilteringNumbers = mutableListOf<PhoneDirectory4>()
+
+    listBeforeFilteringNumbers.add(PhoneDirectory4("Artur2", 88008888888, Company("null")))
+    listBeforeFilteringNumbers.add(PhoneDirectory4("Artur", null, null))
+    listBeforeFilteringNumbers.add(PhoneDirectory4("Artur1", 88009997756, null))
+    listBeforeFilteringNumbers.add(PhoneDirectory4("Artur2", 88008888888, Company("null")))
+    listBeforeFilteringNumbers.add(PhoneDirectory4("Artur3", null, Company("Google")))
+    listBeforeFilteringNumbers.add(PhoneDirectory4("Artur4", 88005554433, Company("Ozon")))
+
+    print(listBeforeFilteringNumbers.filter { it.phoneNumber == null })
+    println()
+
+    val listPhoneNumbers = listBeforeFilteringNumbers.filter { it.phoneNumber != null }
+
+    println(listPhoneNumbers)
+}
