2f16f5f59015b373edb1034c225d27cb5beeb321
Artut <ArtSoull@yandex.ru>
2024-05-11 08:30:00 +0500

    KS-10-1

--- src/main/kotlin/lesson_10/lesson10_task1.kt
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -1,9 +1,9 @@
 package lesson_10
 
 fun main() {
-    val playerThrow = rollingDice()
+    val playerThrow = rollingDice1()
     println("the player's move: $playerThrow")
-    val computerThrow = rollingDice()
+    val computerThrow = rollingDice1()
     println("the computer's move: $computerThrow")
 
     if (playerThrow > computerThrow) println("Humanity has won")
@@ -11,4 +11,4 @@ fun main() {
     else println("The rivalry continues")
 }
 
-fun rollingDice(): Int = (1..6).random()
+fun rollingDice1(): Int = (1..6).random()
