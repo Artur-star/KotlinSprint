72b41d798e46b009d4ff86876f8bd53e4e5d8e0d
Artut <ArtSoull@yandex.ru>
2024-05-12 14:25:57 +0500

    KS-13-1 Fixed

--- src/main/kotlin/lesson_13/lesson13_task1.kt
+++ src/main/kotlin/lesson_13/lesson13_task1.kt
@@ -3,14 +3,14 @@ package lesson_13
 class PhoneDirectory(
     val name: String,
     val phoneNumber: Long,
-    val company: Company?,
+    val company: Company1?,
 ) {
     override fun toString(): String {
         return "PhoneDirectory(name='$name', phoneNumber=$phoneNumber, company=$company)"
     }
 }
 
-class Company
+class Company1
 
 fun main() {
     val phoneDirectory = PhoneDirectory("as", 3232, null)
--- src/main/kotlin/lesson_13/lesson13_task2.kt
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -3,14 +3,14 @@ package lesson_13
 class PhoneDirectory1(
     val name: String,
     val phoneNumber: Long,
-    val company: Company? = null,
+    val company: Company2? = null,
 ) {
     override fun toString(): String {
         return "- name: $name\n- phoneNumber: $phoneNumber\n- company: ${company ?: "<не указано>"}"
     }
 }
 
-class Company
+class Company2
 
 fun main() {
     val phoneDirectory = PhoneDirectory1("Alex", 89998887733)
