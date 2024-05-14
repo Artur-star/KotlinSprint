8182e870fb0ba9e64ccc4d928296ea8337b87a12
Artut <ArtSoull@yandex.ru>
2024-05-14 19:30:33 +0500

    KS-14-3 Fixed1

--- src/main/kotlin/lesson_14/lesson14_task3.kt
+++ src/main/kotlin/lesson_14/lesson14_task3.kt
@@ -1,5 +1,42 @@
 package lesson_14
 
+class Circle(
+    color: String,
+    val radius: Double,
+) : Figure(color) {
+
+    override fun calculateArea(): Double {
+        return Math.PI * radius.times(radius)
+    }
+
+    override fun calculatePerimeter(): Double {
+        return 2 * Math.PI * radius
+    }
+}
+
+abstract class Figure(
+    val color: String,
+) {
+    abstract fun calculateArea(): Double
+
+    abstract fun calculatePerimeter(): Double
+}
+
+class Rectangle(
+    color: String,
+    val width: Double,
+    val height: Double,
+) : Figure(color) {
+
+    override fun calculateArea(): Double {
+        return width * height
+    }
+
+    override fun calculatePerimeter(): Double {
+        return 2 * (width + height)
+    }
+}
+
 fun main() {
     val listFigure = mutableListOf<Figure>()
     listFigure.apply {
