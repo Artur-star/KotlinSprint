026da82ffa1eed7f8c0951bce759f6a6b3d5847a
Artur <ArtSoull@yandex.ru>
2024-06-04 13:27:55 +0500

    KS-16-5 Fixed1

--- src/main/kotlin/lesson_16/lesson16_task5.kt
+++ src/main/kotlin/lesson_16/lesson16_task5.kt
@@ -1,14 +1,18 @@
 package lesson_16
 
+import kotlin.math.abs
+
 class Player(
     private val name: String,
     private var health: Int,
     private val strength: Int,
 ) {
     fun takeDamage(damage: Int) {
+        val absDamage = abs(damage)
         if (playerDeath()) {
-            if (health >= damage) {
-                health -= damage
+            if (health >= absDamage) {
+                health -= absDamage
+                Math.abs(absDamage)
                 println("Игрок ${this.name} получил урон. Здоровья осталось = ${this.health}")
             } else {
                 health = 0
@@ -19,15 +23,15 @@ class Player(
 
     fun getTreatment(treatment: Int) {
         if (playerDeath()) {
-            health += treatment
-            println("Игрок ${this.name} лечиться. Здоровья стало = ${this.health}")
-
-        }
+            if (treatment <= health) {
+                health += treatment
+                println("Игрок ${this.name} лечиться. Здоровья стало = ${this.health}")
+            } else println("Лечение превышает максимальное значение здоровья. Здоровье = ${this.health}")
+        } else health = 0
     }
 
     private fun playerDeath(): Boolean {
         return if (health <= 0) {
-            health = 0
             println("Игрок ${this.name} умер. Его здоровье = ${this.health}")
             false
         } else true
@@ -38,7 +42,7 @@ fun main() {
     val playerArt = Player("Art", 100, 20)
 
     playerArt.takeDamage(50)
-    playerArt.getTreatment(10)
+    playerArt.getTreatment(100)
     playerArt.takeDamage(40)
     playerArt.getTreatment(30)
     playerArt.takeDamage(60)
