3a56f47ec4e55e28b4951dc4689b0abade888bdf
Artur <ArtSoull@yandex.ru>
2024-06-13 21:16:48 +0500

    KS-19-3 Spaceship

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,20 @@
+package lesson_19
+
+class Spaceship() {
+
+    fun fly() {
+        TODO("здесь нужна информация о дополнительной логике")
+    }
+
+    fun shoot() {
+        TODO()
+    }
+
+    fun land() {}
+}
+
+fun main() {
+    val spaceship = Spaceship()
+    spaceship.fly()
+    spaceship.shoot()
+}
