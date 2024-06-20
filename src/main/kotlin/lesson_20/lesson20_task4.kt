6f6a8466e01b20afe3d666daef4aca8913f7ac9a
Artut <ArtSoull@yandex.ru>
2024-06-20 09:26:00 +0500

    KS-20-4 Fixed1

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -3,11 +3,11 @@ package lesson_20
 fun main() {
     val listString: List<String> = listOf("element 0", "element 1", "element 2", "element 3", "element 4", "element 5")
 
-    val map = listString.map { it: String -> "Нажат элемент $it" }
+    val map = listString.map { it: String -> { "Нажат элемент $it" } }
 
     for (i in 0 until map.size) {
         if (i % 2 == 0) {
-            println(map[i])
+            println(map[i].invoke())
         }
     }
 }
