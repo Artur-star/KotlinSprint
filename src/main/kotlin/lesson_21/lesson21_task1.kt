3c6d9c51018b9ec90a851a2b34892908bf0c3c4d
Artut <ArtSoull@yandex.ru>
2024-06-22 08:36:31 +0500

    KS-21-1 Fixed1

--- src/main/kotlin/lesson_21/lesson21_task1.kt
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -2,13 +2,7 @@ package lesson_21
 
 fun String.vowelCount(): Int {
     val vowelsLetters: CharSequence = "eyuioa"
-    var count = 0
-    for (i in 0 until this.length) {
-        if (vowelsLetters.contains(this[i], ignoreCase = true)) {
-            count++
-        }
-    }
-    return count
+    return count { vowelsLetters.contains(it) }
 }
 
 fun main() {
